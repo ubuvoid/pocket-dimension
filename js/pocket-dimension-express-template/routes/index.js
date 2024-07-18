8f5d3182cc74c69c06a75319c5a6955fe235ed86
ubuvoid <73708290+ubuvoid@users.noreply.github.com>
2024-07-18 10:02:46 -0700

    Update index.js - let -> const, more symmetrical comments

--- js/pocket-dimension-express-template/routes/index.js
+++ js/pocket-dimension-express-template/routes/index.js
@@ -18,25 +18,25 @@ router.get('/', function(req, res, next) {
 
 
 // Endpoint for an example REST service.
-// TODO Write IDL (JTD, proto, etc.) for the service method names, requests and responses.
+// TODO Write IDL (JTD, proto, etc.) for the service method requests and responses.
 const serviceHandler = pocket.serviceUtils.buildServiceHandler("FooService");
 
+// TODO Write an actual method handler, populate response_data, and return okStatus().
 const method1Handler = function (app, request_data, response_data) {
-  // TODO Write an actual method handler, populate response_data, and return okStatus()
   return pocket.statusUtils.notImplementedStatus();
 }
 
-// TODO: Generate validator functions from object schemas (IDL) and pass them in here.
+// TODO Generate validator functions from object schemas (IDL) and pass them in here.
 serviceHandler.registerCallback("Method1", method1Handler);
 
 app.post('/foo/method1', async function(req, res, next) {
   try {
     // local service calls are invoked async, to allow method handlers to make async calls as needed.
-    let result = await pocket.serviceUtils.invokeLocalServiceCall(
+    const result = await pocket.serviceUtils.invokeLocalServiceCall(
       req.app, serviceHandler, "Method1", req.body);
     // If no error was thrown, result looks like:
     //  {
-    //    data: {},  // contains response data as populated by the method handler.
+    //    data: {},  // contains response data as populated by the method handler. 
     //    info: {}   // contains status code and message. see statusUtils.js in pocket-dimension-framework.
     //  }
     res.json(result);
